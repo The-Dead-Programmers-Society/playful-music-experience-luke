2aa7932ad08f06215f16c244dcf857e3cf1aa325
LukeR4704 <C23347841@mytudublin.ie>
2024-04-25 23:41:41 +0100

    Final commit

--- Music.gd
+++ Music.gd
@@ -1,11 +1,19 @@
 extends Node2D
 
+@onready var Drum:AudioStreamPlayer2D = $Bodhran/Control/Drum
+@onready var Harp:AudioStreamPlayer2D = $Harp/Harp
 
 func _on_button_pressed():
-	$Bodhran/Control/Drum.play()
+	if Drum.playing:
+		Drum.stop()
+	else:
+		Drum.play()
 
 func _on_button_2_pressed():
 	$"Stick(bodhran)/Stick".play()
 	
 func _on_button_3_pressed():
-	$Harp/Harp.play()
+	if Harp.playing:
+		Harp.stop()
+	else:
+		Harp.play()
